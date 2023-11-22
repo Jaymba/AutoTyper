0d129fdc417f3de5af3b3d823ea254ab628eb035
Jay Stoltzfus <stoltzfusjay86@gmail.com>
2023-11-22 10:24:58 -0500

    Create the main application

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+from functools import partial
 from tkinter import *
 from tkinter import ttk 
+import tkinter as tk
+import time
+from pynput.keyboard import Controller
 
-window = Tk()
-window.title("AutoTyper")
+keyboard = Controller()
 
-frm = ttk.Frame(window, padding=10)
+root = Tk()
+root.title("AutoTyper")
+root.geometry("230x90")
+
+
+frm = ttk.Frame(root, padding=10)
 frm.grid()
 
+for i in range(3):
+    frm.grid_columnconfigure(i,weight=1)
 
-ttk.Button(frm,text="Quit", command=window.destroy).grid(column=3,row=3)
+for i in range(4):
+    frm.grid_rowconfigure(i,weight=0)
+
+ttk.Label(frm,text="Password:").grid(row=0,column=0)
+
+pswd_var = tk.StringVar()
+password = ttk.Entry(frm,textvariable=pswd_var).grid(row=0,column=1,columnspan=3)
+
+def SendKeys(ps):
+    txt = ps.get()
+    root.destroy()
+    time.sleep(2)
+    keyboard.type(txt)
+    
 
 
-window.mainloop()
+ttk.Button(frm,text="Enter", command=partial(SendKeys, pswd_var)).grid(column=2,row=3,sticky=tk.SE, pady=10)
+
+
+root.grid_rowconfigure(5, minsize=41)
+root.grid_columnconfigure(5, minsize=36)
+root.mainloop()
